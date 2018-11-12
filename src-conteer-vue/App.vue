6451a4e5da2f4a6568e72516634561a36f2c4e0a
lianjl <xiaolian951210@163.com>
2018-11-12 21:16:25 +0800

    router路由练习

---
+++ src-conteer-vue/App.vue
@@ -0,0 +1,42 @@
+<template>
+ <div>
+   <h2>clicked:{{counter}}times, count is {{evenOrodd}}</h2>
+   <button @click="increment">+</button>
+   <button @click="decrement">-</button>
+   <button @click="incrementIfOdd">increment if odd </button>
+   <button @click="incrementAsync">increment async</button>
+ </div>
+</template>
+
+<script>
+  export default {
+     data(){
+       return{
+         counter:0,
+         evenOrodd:''
+       }
+     },
+    methods:{
+      increment(){
+        this.counter++
+      },
+      decrement(){
+        this.counter--
+      },
+      incrementIfOdd(){
+        if(this.counter%2===1){
+          this.counter++
+        }
+      },
+      incrementAsync(){
+        setTimeout(()=>{
+          this.counter++
+        },1000)
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
