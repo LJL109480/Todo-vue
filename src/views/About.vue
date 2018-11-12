1d8f96ef1910476cd982ca51ade532bad0589118
lianjl <xiaolian951210@163.com>
2018-11-12 20:59:01 +0800

    router路由练习

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,9 +1,16 @@
 <template>
-  <div>About</div>
+  <div>About
+    <h3>msg:{{msg}}</h3>
+    <input type="text">
+  </div>
+
+
 </template>
 <script>
   export default{
-
+   props:{
+     msg:String
+   }
   }
 </script>
 <style>
